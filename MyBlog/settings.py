47c1d0a5d2a455977c2577b4d457be6c1942f530
Rus_PC <azazelo1993@gmail.com>
2020-11-28 23:16:00 +0300

    Add post model

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'event.apps.EventConfig',
     'django.contrib.admin',
     'django.contrib.auth',
