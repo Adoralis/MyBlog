263398207f409b47baaa409737b4281b3722f9a9
Rus_PC <azazelo1993@gmail.com>
2020-11-30 00:16:40 +0300

    ADD BOOTSTRAP

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+import event.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', event.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
